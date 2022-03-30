f620eabb891c7fa9c9c49df1fc6dc63160e852fd
Gennady Badinov <gennady.badinov@gmail.com>
2022-03-30 23:06:31 +0300

    Fix `Allergy` plural name in migrations

--- recipes/migrations/0005_allergy_remove_subscription_allergies_and_more.py
+++ recipes/migrations/0005_allergy_remove_subscription_allergies_and_more.py
@@ -6,30 +6,55 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('recipes', '0004_recipe_calories_recipe_carbohydrates_recipe_fats_and_more'),
+        ("recipes", "0004_recipe_calories_recipe_carbohydrates_recipe_fats_and_more"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Allergy',
+            name="Allergy",
+            options={"verbose_name_plural": "Allergies"},
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=20, verbose_name='Allergy name')),
-                ('description', models.CharField(blank=True, max_length=200, null=True, verbose_name='Allergy descriptiom')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=20, verbose_name="Allergy name")),
+                (
+                    "description",
+                    models.CharField(
+                        blank=True,
+                        max_length=200,
+                        null=True,
+                        verbose_name="Allergy descriptiom",
+                    ),
+                ),
             ],
         ),
         migrations.RemoveField(
-            model_name='subscription',
-            name='allergies',
+            model_name="subscription",
+            name="allergies",
         ),
         migrations.AddField(
-            model_name='ingredient',
-            name='allergy',
-            field=models.ManyToManyField(related_name='ingredients', to='recipes.allergy', verbose_name='Ingredient allergy'),
+            model_name="ingredient",
+            name="allergy",
+            field=models.ManyToManyField(
+                related_name="ingredients",
+                to="recipes.allergy",
+                verbose_name="Ingredient allergy",
+            ),
         ),
         migrations.AddField(
-            model_name='subscription',
-            name='allergy',
-            field=models.ManyToManyField(related_name='subscriptions', to='recipes.allergy', verbose_name='Subscription allergies list'),
+            model_name="subscription",
+            name="allergy",
+            field=models.ManyToManyField(
+                related_name="subscriptions",
+                to="recipes.allergy",
+                verbose_name="Subscription allergies list",
+            ),
         ),
     ]
