8cb2ec1e969d1cb33274f24ec52fec2c8dacd68f
Gennady Badinov <gennady.badinov@gmail.com>
2022-03-30 12:09:17 +0300

    Use `phonenumber_field` for user phone validation

--- dvmn_recipes/settings.py
+++ dvmn_recipes/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "phonenumber_field",
     "recipes.apps.RecipesConfig",
     "django.contrib.admin",
     "django.contrib.auth",
