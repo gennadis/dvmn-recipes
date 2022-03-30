7295f4b808cb523063d4921a6e48e62292919eb9
Gennady Badinov <gennady.badinov@gmail.com>
2022-03-30 09:53:09 +0300

    Display `ingredient amounts` with tabular  inlines

--- recipes/admin.py
+++ recipes/admin.py
@@ -4,6 +4,21 @@ from django.contrib import admin
 from .models import Recipe, Ingredient, MealType, RecipeIngredientAmount
 
 
-@admin.register(Recipe, Ingredient, MealType, RecipeIngredientAmount)
+class RecipeIngredientAmountInline(admin.TabularInline):
+    model = RecipeIngredientAmount
+    verbose_name = "ingredient amount"
+
+
+@admin.register(Recipe)
+class RecipesAdmin(admin.ModelAdmin):
+    inlines = (RecipeIngredientAmountInline,)
+
+
+@admin.register(Ingredient)
+class RecipesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MealType)
 class RecipesAdmin(admin.ModelAdmin):
     pass
