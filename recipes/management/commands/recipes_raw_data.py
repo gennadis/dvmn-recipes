5913007fc0a27eebad71d31660eab65aa65fe590
Gennady Badinov <gennady.badinov@gmail.com>
2022-03-30 15:20:50 +0300

    Add recipes raw data for `fixtures` generation

---
+++ recipes/management/commands/recipes_raw_data.py
@@ -0,0 +1,56 @@
+# MEAL TYPES
+meal_types_names = [
+    "appetizers and snaks",
+    "baked goods",
+    "breakfast and brunch",
+    "dessert",
+    "quick and easy",
+    "lunch",
+    "main dish",
+    "salad",
+    "side dish",
+    "slow cooker",
+    "soups and stews",
+    "special occasions",
+]
+
+# INGREDIENTS
+gramm = "gramms"
+cup = "cups"
+teaspoon = "teaspoon"
+piece = "piece"
+milliliter = "milliliters"
+
+ingredients_pairs = [
+    ("mozzarella cheese", gramm),
+    ("garlic", piece),
+    ("cream", milliliter),
+    ("milk", milliliter),
+    ("yoghurt", milliliter),
+    ("egg", piece),
+    ("sunflower oil", milliliter),
+    ("peanut oil", milliliter),
+    ("orange", piece),
+    ("lemon", piece),
+    ("mango", piece),
+    ("cacao", teaspoon),
+    ("sugar", teaspoon),
+    ("mint", piece),
+    ("chicken", gramm),
+    ("beef", gramm),
+    ("minced meat", gramm),
+    ("salmon", gramm),
+    ("rice", gramm),
+    ("pasta", gramm),
+    ("white beans", gramm),
+    ("honey", teaspoon),
+    ("chili", piece),
+    ("mushrooms", gramm),
+    ("potatoes", gramm),
+    ("carrots", piece),
+    ("tomatoes", piece),
+    ("cucumber", piece),
+    ("onion", piece),
+    ("avocado", piece),
+    ("spinach", piece),
+]
