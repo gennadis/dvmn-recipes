76c2a9140a72cf3b722eabdeec3318f947f09355
ilyashirko <ilyashirko@gmail.com>
2022-03-31 13:02:03 +0300

    temporary file

---
+++ bot_processing.py
@@ -0,0 +1,30 @@
+
+
+
+def get_user_info(user_telegram_id):
+
+    # return User object if actual and None if not
+
+    pass
+
+
+def save_profile(profile_info):
+
+    # just for example
+    # profile_info = (id, username, name, surname, phone)  everything is STRING
+    # return True if successful saving
+    # return False or raise exceptions
+
+    pass
+
+def get_user_subscriptions(user_telegram_id):
+
+    # return list of user subscriptions titles
+    # ex ["Первая подписка", "Вторая подписка", ...]
+
+    pass
+
+def get_dish(subscription_title):
+
+    # return dict or model with recipe 
+
