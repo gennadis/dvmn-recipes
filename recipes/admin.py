2aaaeb4b9773746f5f24fee68123915294bae7ad
Gennady Badinov <gennady.badinov@gmail.com>
2022-04-03 21:44:23 +0300

    Enrich admin panel with additional information

--- recipes/admin.py
+++ recipes/admin.py
@@ -27,26 +27,27 @@ class RecipeStepInline(admin.TabularInline):
 @admin.register(Recipe)
 class RecipesAdmin(admin.ModelAdmin):
     inlines = (RecipeIngredientAmountInline, RecipeStepInline)
+    list_display = ("name", "get_recipe_meal_types", "get_recipe_allergies")
 
 
 @admin.register(Ingredient)
 class RecipesAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "unit", "get_ingredient_allergies")
 
 
 @admin.register(MealType)
 class RecipesAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "get_recipes_count")
 
 
 @admin.register(TelegramUser)
 class RecipesAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("get_telegram_username", "phone_number", "first_name", "last_name")
 
 
 @admin.register(Subscription)
 class RecipesAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("owner", "name", "plan", "meal_type", "get_subscription_allergies")
 
 
 @admin.register(Allergy)
@@ -56,9 +57,15 @@ class RecipesAdmin(admin.ModelAdmin):
 
 @admin.register(PromoCode)
 class RecipesAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("code", "discount", "valid_thru", "get_activation_count")
 
 
 @admin.register(SubscriptionPlan)
 class RecipesAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "name",
+        "price",
+        "currency",
+        "duration",
+        "get_plan_activation_count",
+    )
